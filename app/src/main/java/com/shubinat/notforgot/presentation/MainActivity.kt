43f7d4dd7eeed9facc4b1e3effe1f37528233dcc
Shubinat <shubinat729@gmail.com>
2022-11-28 01:05:50 +0300

    Initial Commit

---
+++ app/src/main/java/com/shubinat/notforgot/presentation/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.shubinat.notforgot.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.shubinat.notforgot.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
