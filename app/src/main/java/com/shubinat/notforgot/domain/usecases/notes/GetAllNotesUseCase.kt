4d9675bffe310c05b90b36d9a71d6e8ec78e3a63
Shubinat <shubinat729@gmail.com>
2023-01-04 21:55:59 +0300

    Added Coroutines on MainFragment.kt

--- app/src/main/java/com/shubinat/notforgot/domain/usecases/notes/GetAllNotesUseCase.kt
+++ app/src/main/java/com/shubinat/notforgot/domain/usecases/notes/GetAllNotesUseCase.kt
@@ -3,10 +3,15 @@ package com.shubinat.notforgot.domain.usecases.notes
 import com.shubinat.notforgot.domain.entity.Note
 import com.shubinat.notforgot.domain.entity.User
 import com.shubinat.notforgot.domain.repository.NoteRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.withContext
 
 class GetAllNotesUseCase(private val repository: NoteRepository) {
 
-    operator fun invoke(user: User) : List<Note> {
-        return repository.getNotes(user)
+    suspend operator fun invoke(user: User): List<Note> {
+        return withContext(Dispatchers.IO) {
+            repository.getNotes(user)
+        }
     }
 }
