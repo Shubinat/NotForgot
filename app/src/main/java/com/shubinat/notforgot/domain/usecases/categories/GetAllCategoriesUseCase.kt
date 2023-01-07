5219790dde1393f291709cabf88eebb3db083653
Shubinat <shubinat729@gmail.com>
2023-01-07 20:31:28 +0300

    Added Blocking Coroutines

--- app/src/main/java/com/shubinat/notforgot/domain/usecases/categories/GetAllCategoriesUseCase.kt
+++ app/src/main/java/com/shubinat/notforgot/domain/usecases/categories/GetAllCategoriesUseCase.kt
@@ -3,9 +3,13 @@ package com.shubinat.notforgot.domain.usecases.categories
 import com.shubinat.notforgot.domain.entity.Category
 import com.shubinat.notforgot.domain.entity.User
 import com.shubinat.notforgot.domain.repository.CategoryRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 
 class GetAllCategoriesUseCase(private val repository: CategoryRepository) {
-     operator fun invoke(user: User) : List<Category> {
-        return repository.getCategories(user)
+    suspend operator fun invoke(user: User): List<Category> {
+        return withContext(Dispatchers.IO) {
+            repository.getCategories(user)
+        }
     }
 }
